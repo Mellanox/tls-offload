6ff66ff8edc16fd885f41bf6a3d844e4e87c74e8
Ilan Tayari <ilant@mellanox.com>
2016-06-30 09:58:27 +0300

    mlx_accel_core: Using new FPGA access reg register
    Change-Id: I88568974b066413dfef3c680f2f5b6c17e5d140f
    Signed-off-by: Ilan Tayari <ilant@mellanox.com>
    Issue: None

--- drivers/net/ethernet/mellanox/accelerator/core/accel_core_hw.c
+++ drivers/net/ethernet/mellanox/accelerator/core/accel_core_hw.c
@@ -33,59 +33,20 @@
 #include "accel_core.h"
 #include <linux/mlx5/device.h>
 
-static int mlx_accel_access_fnvcra(struct mlx5_core_dev *dev,
-				   u8 size, u64 addr, u8 *buf, int write)
-{
-	u32 in[MLX5_ST_SZ_DW(fnvcra_reg)];
-	u32 out[MLX5_ST_SZ_DW(fnvcra_reg)];
-	u32 addr_high = addr >> 32;
-	u32 addr_low = addr & 0xffffffff;
-	int err, i;
-
-	/* check DWORD alignment both for address and size */
-	if (((size & 0x3) != 0) || ((addr_low & 0x3) != 0))
-		return -EINVAL;
-
-	memset(in, 0, sizeof(in));
-	memset(out, 0, sizeof(out));
-
-	pr_debug("FNVCRA %u bytes at 0x%llx write?%d\n", size, addr, write);
-
-	MLX5_SET(fnvcra_reg, in, size, size);
-	MLX5_SET(fnvcra_reg, in, address_63_32, addr_high);
-	MLX5_SET(fnvcra_reg, in, address_31_0, addr_low);
-	if (write) {
-		for (i = 0; i < size; ++i)
-			MLX5_SET(fnvcra_reg, in, data[i], buf[i]);
-	}
-
-	err = mlx5_core_access_reg(dev, in, sizeof(in), out, sizeof(out),
-				   MLX5_REG_FNVCRA, 0, !!write);
-	if (err)
-		return err;
-
-	if (!write) {
-		for (i = 0; i < size; i++)
-			buf[i] = MLX5_GET(fnvcra_reg, out, data[i]);
-	}
-
-	return 0;
-}
-
 int mlx_accel_read_i2c(struct mlx5_core_dev *dev,
 		       size_t size, u64 addr, u8 *buf)
 {
 	u8 actual_size;
 	size_t bytes_done = 0;
+	size_t max_size = MLX5_FPGA_ACCESS_REG_SIZE_MAX;
 	int rc;
 
 	while (bytes_done < size) {
-		actual_size = min(MLX5_FLD_SZ_BYTES(fnvcra_reg, data),
-				  (size - bytes_done));
+		actual_size = min(max_size, (size - bytes_done));
 
-		rc = mlx_accel_access_fnvcra(dev, actual_size,
-					     addr + bytes_done,
-					     buf + bytes_done, 0);
+		rc = mlx5_fpga_access_reg(dev, actual_size,
+					  addr + bytes_done,
+					  buf + bytes_done, 0);
 		if (rc) {
 			pr_err("Failed to read FPGA crspace data for %s\n",
 			       dev_name(&dev->pdev->dev));
@@ -103,15 +64,15 @@ int mlx_accel_write_i2c(struct mlx5_core_dev *dev,
 {
 	u8 actual_size;
 	size_t bytes_done = 0;
+	size_t max_size = MLX5_FPGA_ACCESS_REG_SIZE_MAX;
 	int rc;
 
 	while (bytes_done < size) {
-		actual_size = min(MLX5_FLD_SZ_BYTES(fnvcra_reg, data),
-				  (size - bytes_done));
+		actual_size = min(max_size, (size - bytes_done));
 
-		rc = mlx_accel_access_fnvcra(dev, actual_size,
-					     addr + bytes_done,
-					     buf + bytes_done, 1);
+		rc = mlx5_fpga_access_reg(dev, actual_size,
+					  addr + bytes_done,
+					  buf + bytes_done, 1);
 		if (rc) {
 			pr_err("Failed to write FPGA crspace data for %s\n",
 			       dev_name(&dev->pdev->dev));
