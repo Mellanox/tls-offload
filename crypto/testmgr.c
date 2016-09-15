4003ad162139869a6301d21b43a5fa685648e21f
Dave Watson <davejwatson@fb.com>
2016-09-15 10:31:54 -0700

    crypto: Add gcm template for rfc5288
    AAD data length is 13 bytes, tag is 16.
    
    Issue: None
    Change-Id: Ib7605b022b050c3cf8370f4817b935ca0ad42711
    Signed-off-by: Dave Watson <davejwatson@fb.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3748,6 +3748,22 @@ static const struct alg_test_desc alg_test_descs[] = {
 			}
 		}
 	}, {
+		.alg = "rfc5288(gcm(aes))",
+		.test = alg_test_aead,
+		.fips_allowed = 1,
+		.suite = {
+			.aead = {
+				.enc = {
+					.vecs = aes_gcm_rfc5288_enc_tv_template,
+					.count = AES_GCM_5288_ENC_TEST_VECTORS
+				},
+				.dec = {
+					.vecs = aes_gcm_rfc5288_dec_tv_template,
+					.count = AES_GCM_5288_DEC_TEST_VECTORS
+				}
+			}
+		}
+	}, {
 		.alg = "rfc7539(chacha20,poly1305)",
 		.test = alg_test_aead,
 		.suite = {
