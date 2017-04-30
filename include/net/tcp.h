88d427cc81cc614bd1bc0523e684b827394e3d40
Boris Pismenny <borisp@mellanox.com>
2017-04-30 11:00:21 +0300

    tcp: ULP infrastructure
    Add the infrustructure for attaching user level protocols over
    TCP sockets. Based on a similar infrastructure in tcp_cong.
    
    Change-Id: I094bd477e64f544ba7db09b1b7da94b362a9d806
    Signed-off-by: Boris Pismenny <borisp@mellanox.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1956,4 +1956,29 @@ static inline void tcp_listendrop(const struct sock *sk)
 	__NET_INC_STATS(sock_net(sk), LINUX_MIB_LISTENDROPS);
 }
 
+/*
+ * Interface for adding Upper Level Protocols over TCP
+ */
+
+#define TCP_ULP_NAME_MAX	16
+#define TCP_ULP_MAX		128
+#define TCP_ULP_BUF_MAX		(TCP_ULP_NAME_MAX*TCP_ULP_MAX)
+
+struct tcp_ulp_ops {
+	struct list_head	list;
+
+	/* initialize ulp */
+	int (*init)(struct sock *sk);
+	/* cleanup ulp */
+	void (*release)(struct sock *sk);
+
+	char 		name[TCP_ULP_NAME_MAX];
+	struct module 	*owner;
+};
+int tcp_register_ulp(struct tcp_ulp_ops *type);
+void tcp_unregister_ulp(struct tcp_ulp_ops *type);
+int tcp_set_ulp(struct sock *sk, const char *name);
+void tcp_get_available_ulp(char *buf, size_t len);
+void tcp_cleanup_ulp(struct sock *sk);
+
 #endif	/* _TCP_H */
